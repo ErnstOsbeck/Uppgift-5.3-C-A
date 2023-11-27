b70f93349f8bd5bf67e7a5e47b835646b3cbc695
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-27 17:02:57 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.3 C-A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift_5._3_C_A
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = { "Vad heter Ernst", "Vad heter Johan", "Vad heter Zache", "Vad heter Alexander" };
+            string[] rättsvar = { "ernst", "johan", "zache", "alexander" };
+            Console.WriteLine("Skriv ett tal 1-4");
+            int val = int.Parse(Console.ReadLine());
+            Console.WriteLine(frågor[val-1]);
+            string svar = Console.ReadLine();
+            if (svar.ToLower() == rättsvar[val-1])
+            {
+                Console.WriteLine("Du svarade rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Du svarade fel");
+            }
+        }
+    }
+}
